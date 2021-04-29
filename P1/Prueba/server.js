ba4d0bb3405158522d0f842a5deb428aa1099b25
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-04-29 21:55:14 +0200

    Continuacion

--- P1/Prueba/server.js
+++ P1/Prueba/server.js
@@ -1,22 +1,22 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const PUERTO = 9000;
+const PUERTO = 9000
 
+const mime = {
+    'html' : 'text/html',
+    'css'  : 'text/css',
+    'jpg'  : 'image/jpg',
+    'png'  : 'image/png',
+   };
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("----------> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+  var q = url.parse(req.url, true);
+  console.log("Recurso:" + q.pathname)
 
+  var filename = "." + q.pathname;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
