21bd817dc7c77eb90216493815b2f7702a21a3ae
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-05-05 09:48:43 +0200

    Index

--- P1/Prueba/server.js
+++ P1/Prueba/server.js
@@ -4,36 +4,46 @@ const fs = require('fs');
 const PUERTO = 9000
 
 const mime = {
-    'html' : 'text/html',
-    'css'  : 'text/css',
-    'jpg'  : 'image/jpg',
-    'png'  : 'image/png',
-   };
+  'html' : 'text/html',
+  'css'  : 'text/css',
+  'jpg'  : 'image/jpg',
+  'png'  : 'image/png',
+ };
+
 //-- Configurar y lanzar el servidor. Por cada peticion recibida
 //-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
   console.log("----------> Peticion recibida")
-  var q = url.parse(req.url, true);
   console.log("Recurso solicitado (URL): " + req.url)
+  var q = url.parse(req.url, true);
   console.log("Recurso:" + q.pathname)
 
-  var filename = "";
+  var filename = "." + q.pathname;
 
   //-- Obtener fichero a devolver
   if (q.pathname == "/")
     filename += "index.html"
-  else {
-    filename = q.pathname;
-  }
 
   //-- Leer fichero
   fs.readFile(filename, function(error, data) {
 
-    //-- Mensaje de error
+    //-- Fichero no encontrado. Devolver mensaje de error
     if (error) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       res.write("Error 404 Not Found");
       return res.end();
     }else{
 
-   
+    //-- Tipo mime por defecto: html
+    //let mime = "text/html"
+
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+  }});
+
+}).listen(PUERTO);
+
+console.log("Ejecutando Server...")
+console.log("Puerto: " + PUERTO)
