1d7530e89c4dd80c09a1d4783f178302eaecb749
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-05-31 17:51:51 +0200

    main

--- P4/main.js
+++ P4/main.js
@@ -5,12 +5,9 @@ const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
 console.log("Arrancando electron...")
 
-//-- Punto de entrada. En cuanto electron está listo,
-//-- ejecuta esta función
 info1.textContent = process.versions.node;
 info2.textContent = process.versions.chrome;
 info3.textContent = process.versions.electron;
-info7.textContent = ip.address() + ":9000/chat.html";
 electron.app.on('ready', ()=>{
   console.log("Evento Ready!")
 
@@ -19,13 +16,5 @@ electron.app.on('ready', ()=>{
     width: 600,
     height: 400
   })
-
-  //-- En la parte superior se nos ha creado el menu
-  //-- por defecto
-  //-- Si lo queremos quitar, hay que añadir esta línea
-  //win.setMenuBarVisibility(false)
-
-  //-- Cargar la interfaz gráfica, que se encuentra en el
-  //-- fichero index.html
   win.loadFile('index.html')
 })
