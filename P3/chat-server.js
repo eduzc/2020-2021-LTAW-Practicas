d514cc9377ea15fd41830dbe7a3a0480ddef718a
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-06-01 21:19:47 +0200

    comandos especiales1

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PUERTO = 9090
+const PUERTO = 9000
 // En caso de tener la carpeta de node funcionaría el audio
 // let tututun = new Audio('tututun.mp3');
 
@@ -47,5 +47,44 @@ io.on('connection', function(socket){
       socket.on('new_message', msg => {
       //tututun.play();
  
-      });
-    })})
+      //  Notificamos en consola el mensaje recibido 
+      console.log("Mensaje recibido: " + msg)
+ 
+      // Definicion de comandos 
+      switch (msg) {
+        case "/help":
+          msg = 'Necesitas ayuda de CHUCK NORRIS?' + "<br>" +
+              '/list : Devuelve el número de usuarios conectados' + "<br>" +
+              '/hello : Devuelve un saludo de CHUCK NORRIS' + "<br>" +
+              '/date : Devuelve la fecha actual';
+          io.emit('new_message', msg);
+          break;
+        case "/hello":
+          msg = 'CHUCK_NORRIS: Bienvenido a mi chat';
+          io.emit('new_message', msg);
+          break
+        case "/date":
+          let d = new Date();
+          let yy = d.getFullYear();
+          let mm = d.getMonth();
+          let dd = d.getDate();
+          msg = 'SERVER: ' + dd + '/' + mm + '/' + yy;
+          io.emit('new_message', msg);
+          break
+        case "/list":
+          msg = 'Número de usuarios conectados: ' + n_usuarios;
+          io.emit('new_message', msg);
+          break
+        default:
+          let d2 = new Date();
+          let h = d2.getHours();
+          let m = d2.getMinutes();
+          let s = d2.getSeconds();
+          let time = '[' + h + ':' + m + ':' + s +']'
+          io.emit('new_message', time + username +': ' + msg);
+      }
+    });
+    
+  });
+});
+ 
