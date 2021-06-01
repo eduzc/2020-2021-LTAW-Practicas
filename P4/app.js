5eca9deb888c70ac04c51b6a69ffc4576d754a1f
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-06-01 08:00:24 +0200

    configuracion de app

--- P4/app.js
+++ P4/app.js
@@ -4,3 +4,36 @@ const v_node = document.getElementById("info1");
 const v_chrome = document.getElementById("info2");
 const v_electron = document.getElementById("info3");
 
+function main()
+{
+
+  // Obtener los elementos del interfaz
+  const display = document.getElementById('display');
+  const msg = document.getElementById("msg");
+  const send = document.getElementById('send');
+
+  // Mensaje a enviar
+
+  msg.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("send").click()
+      
+    }
+  });
+
+  // Envio de mensajes y autoborrado
+  send.onclick = () => {
+    socket.emit('new_message', msg.value);
+    console.log("Mensaje mandado")
+    msg.value="";
+  }
+  
+  // Mostrar mensaje
+  socket.on('new_message', msg => {
+    display.innerHTML += msg + '<br>';    
+    console.log("Mensaje recibido: " + msg)
+  });
+
+
+}
